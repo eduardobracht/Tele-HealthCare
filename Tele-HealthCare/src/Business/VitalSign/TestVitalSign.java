4a21c8c75b1db4c75c5102e12a28c63c2ce66f84
13180606 <13180606@W3152378.portoalegre.pucrsnet.br>
2016-11-10 22:42:14 -0200

    Camada de persistencia: pacote, dao, dto, daodtoderby. falta terminar a daodtoderby e criar o banco

---
+++ Tele-HealthCare/src/Business/VitalSign/TestVitalSign.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.VitalSign;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author 09100324
+ */
+public class TestVitalSign {
+        
+    public void testValidRespiratoryRate() {
+        VitalSign v = new VitalSign(100,120,12,90,"2016-05-20 11:00:05","Sinais vitais normais");
+        
+        
+        /*
+        
+        private int respiratoryRate;
+        private int heartRate;
+        private int bloodPressure;
+        private int weight;
+        private String timestamp;
+        private String statusReport;
+        
+        Consumer c = new Consumer("001", "Cersei", "fogovivo", 
+				"a34ab54", "cersei@got.com", "+11 11 8080-89765",
+				"Rainha Regente", false);
+		String actual = c.getId();
+		String expected = "001";
+		assertEquals(expected, actual);
+        */
+
+    }
+
+
+    public void testValidHeartRate() {
+
+    }
+
+    public void testValidBloodPressure() {
+
+    }
+
+    public void testValidWeight() {
+
+    }
+
+
+    public void testValidTimestamp() {
+
+    }
+
+    public void testValidStatusReport() {
+
+    }
+
+}
